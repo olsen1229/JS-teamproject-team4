07bdd0e6c5b2073cdef38a4bd5a8be4ee816893d
valeeriiiee <obleavalerie@gmail.com>
2024-04-06 00:17:15 +0800

    edited js files added css file and images

--- src/js/discovery-api.js
+++ src/js/discovery-api.js
@@ -4,8 +4,38 @@ const API_KEY = "QRL9keS5G11LcsOGpjLTEnBcqGOFb3Rn";
 export const options = {
     params: {
         apikey: API_KEY,
-        countryCode: "US",
-        keyword: "",
         classificationName: "music",
+        page: 0,
+        size: 16,
     },
 };
+
+export function fetchMusicEvents() {
+    return fetch(`${BASE_URL}classificationName=music&apikey=${API_KEY}`).then((res) => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        } else {
+            return res.json();
+        }
+    });      
+}
+
+export function fetchMusicEventsByKeyword(keyword) {
+    return fetch(`${BASE_URL}classificationName=music&keyword=${keyword}&apikey=${API_KEY}`).then((res) => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        } else {
+            return res.json();
+        }
+    }); 
+}
+
+export function fetchMusicEventsByCountry(countryCode) {
+    return fetch(`${BASE_URL}classificationName=music&countryCode=${countryCode}&apikey=${API_KEY}`).then((res) => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        } else {
+            return res.json();
+        }
+    }); 
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-import { BASE_URL, options } from './discovery-api.js';
+import { fetchMusicEvents, fetchMusicEventsByKeyword, fetchMusicEventsByCountry } from "./discovery-api.js";
+
+const keywordSearchEl = document.querySelector(".search-input");
+const searchFormEl = document.querySelector(".search-form");
+const countrySelectEl = document.querySelector(".country-select");
+
+// COUNTRIEAS APPEAR
+function displayCountries() { 
+    let countries = fetchMusicEventsByCountry()
+    console.log(countries);
+}
+
+displayEventsByCountry();
